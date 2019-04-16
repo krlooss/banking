3000ef4efda8d3a2126ea4e8114f2bf8e8cdaca2
krlooss <krlooss@gmail.com>
2019-04-16 21:53:20 +0200

    restoring savings account

--- src/main/java/banking/SavingsAccount.java
+++ src/main/java/banking/SavingsAccount.java
@@ -1,4 +1,18 @@
 package banking;
 
-public class SavingsAccount {
+public class SavingsAccount extends AbstractAccount {
+    @Override
+    public boolean deposit(double expected) {
+        return false;
+    }
+
+    @Override
+    public double getBalance() {
+        return 0;
+    }
+
+    @Override
+    public boolean withdraw(double v) {
+        return false;
+    }
 }
